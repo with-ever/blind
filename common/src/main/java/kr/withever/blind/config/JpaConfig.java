1502e98b5d1a721cd7d769190643f61772455b0f
hyunseunglee <hs85699104@gmail.com>
2019-07-25 02:12:42 +0900

    JPA config 추가

---
+++ common/src/main/java/kr/withever/blind/config/JpaConfig.java
@@ -0,0 +1,51 @@
+package kr.withever.blind.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+@Configuration
+@EnableTransactionManagement
+public class JpaConfig {
+
+    @Primary
+    @Bean(name = "dataSource")
+    @ConfigurationProperties("spring.datasource.blind")
+        public DataSource dataSource() {
+
+            return DataSourceBuilder.create().type(HikariDataSource.class).build();
+        }
+
+        @Primary
+        @Bean(name = "entityManagerFactory")
+        public LocalContainerEntityManagerFactoryBean entityManagerFactory(
+
+                EntityManagerFactoryBuilder builder,
+                @Qualifier("dataSource") DataSource dataSource) {
+
+        return builder.dataSource(dataSource).packages("kr.withever.blind.domain").build();
+    }
+
+    @Bean(name = "transactionManager")
+    public PlatformTransactionManager transactionManager(
+
+            @Qualifier("entityManagerFactory") EntityManagerFactory entityManagerFactory) {
+
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
+
+        return transactionManager;
+    }
+}
