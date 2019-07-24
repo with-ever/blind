2466c15b3fd913e4f360f8bf84411f2a72876abf
hyunseunglee <hs85699104@gmail.com>
2019-07-25 04:36:06 +0900

    MemberController 수정  - memberRepository Autowired 추가, insertMember() 추가

--- api/src/main/java/kr/withever/blind/api/member/MemberController.java
+++ api/src/main/java/kr/withever/blind/api/member/MemberController.java
@@ -1,12 +1,31 @@
 package kr.withever.blind.api.member;
 
 import kr.withever.blind.advertisement.Advertisement;
+import kr.withever.blind.domain.Member;
+import kr.withever.blind.repo.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping({"/member"})
 public class MemberController {
+
+    @Autowired
+    MemberRepository memberRepository;
 
     public void getMember() {
         Advertisement advertisement = new Advertisement();
+    }
+
+    @RequestMapping("/insert")
+    public void insertMember(@RequestParam String str) {
+
+        Member member = new Member();
+        member.setEmail("test");
+
     }
 }
