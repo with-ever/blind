b3e9d1d3c1b97e070a3ed1b72b4083780529fb13
hyunseunglee <hs85699104@gmail.com>
2019-07-25 04:36:43 +0900

    ApiApplication ComponentScan 추가

--- api/src/main/java/kr/withever/blind/api/ApiApplication.java
+++ api/src/main/java/kr/withever/blind/api/ApiApplication.java
@@ -2,8 +2,10 @@ package kr.withever.blind.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"kr.withever.blind"})
 public class ApiApplication {
 
     public static void main(String[] args) {
