b5c0ffa14a8ce0573cfa0f36bcb3eed034c26ac3
hyunseunglee <hs85699104@gmail.com>
2019-07-25 02:15:14 +0900

    MemberRepository 추가

---
+++ common/src/main/java/kr/withever/blind/repo/MemberRepository.java
@@ -0,0 +1,9 @@
+package kr.withever.blind.repo;
+
+import kr.withever.blind.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
