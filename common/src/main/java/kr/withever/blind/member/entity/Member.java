404da6e6d70c8cd090b12614eedcafc5ff757f9d
youngjinkim <youngjinguy@gmail.com>
2019-12-19 21:41:56 +0900

    :hammer: PR 테스트

--- common/src/main/java/kr/withever/blind/member/entity/Member.java
+++ common/src/main/java/kr/withever/blind/member/entity/Member.java
@@ -1,9 +1,16 @@
 package kr.withever.blind.member.entity;
 
-import lombok.*;
-
-import javax.persistence.*;
 import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "member")
@@ -33,5 +40,7 @@ public class Member {
   private String secessionYn;
   private String stopYn;
   private int reportCnt;
+
+  private String test;
 
 }
